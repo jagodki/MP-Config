b8dc068e50c8627909d36a45208e2e4f08ca0571
Christoph <jagodki.cj@gmail.com>
2026-06-09 11:56:45 +0200

    Update data_structure_algorithm.py
    add a new input parameter using QgsProcessingParameterProviderConnection

--- mp_config/processing_algorithms/data_structure_algorithm.py
+++ mp_config/processing_algorithms/data_structure_algorithm.py
@@ -35,7 +35,8 @@ from qgis.core import (QgsProcessing,
                        QgsFeatureSink,
                        QgsProcessingAlgorithm,
                        QgsProcessingParameterFeatureSource,
-                       QgsProcessingParameterFeatureSink)
+                       QgsProcessingParameterFeatureSink,
+                       QgsProcessingParameterProviderConnection)
 
 
 class DataStructureAlgorithm(QgsProcessingAlgorithm):
@@ -55,9 +56,8 @@ class DataStructureAlgorithm(QgsProcessingAlgorithm):
     # Constants used to refer to parameters and outputs. They will be
     # used when calling the algorithm from another algorithm, or when
     # calling from the QGIS console.
-
-    OUTPUT = 'OUTPUT'
-    INPUT = 'INPUT'
+    
+    CONNECTION = 'CONNECTION'
 
     def initAlgorithm(self, config):
         """
@@ -68,20 +68,11 @@ class DataStructureAlgorithm(QgsProcessingAlgorithm):
         # We add the input vector features source. It can have any kind of
         # geometry.
         self.addParameter(
-            QgsProcessingParameterFeatureSource(
-                self.INPUT,
-                self.tr('Input layer'),
-                [QgsProcessing.TypeVectorAnyGeometry]
-            )
-        )
-
-        # We add a feature sink in which to store our processed features (this
-        # usually takes the form of a newly created vector layer when the
-        # algorithm is run in QGIS).
-        self.addParameter(
-            QgsProcessingParameterFeatureSink(
-                self.OUTPUT,
-                self.tr('Output layer')
+            QgsProcessingParameterProviderConnection(
+                self.CONNECTION,
+                self.tr('Datenbankverbindung'),
+                provider='postgres',  # Standardmäßig PostgreSQL, aber GeoPackage wird auch unterstützt
+                optional=False
             )
         )
 
@@ -93,33 +84,34 @@ class DataStructureAlgorithm(QgsProcessingAlgorithm):
         # Retrieve the feature source and sink. The 'dest_id' variable is used
         # to uniquely identify the feature sink, and must be included in the
         # dictionary returned by the processAlgorithm function.
-        source = self.parameterAsSource(parameters, self.INPUT, context)
-        (sink, dest_id) = self.parameterAsSink(parameters, self.OUTPUT,
-                context, source.fields(), source.wkbType(), source.sourceCrs())
+        # source = self.parameterAsSource(parameters, self.INPUT, context)
+        # (sink, dest_id) = self.parameterAsSink(parameters, self.OUTPUT,
+                # context, source.fields(), source.wkbType(), source.sourceCrs())
 
         # Compute the number of steps to display within the progress bar and
         # get features from source
-        total = 100.0 / source.featureCount() if source.featureCount() else 0
-        features = source.getFeatures()
+        # total = 100.0 / source.featureCount() if source.featureCount() else 0
+        # features = source.getFeatures()
 
-        for current, feature in enumerate(features):
-            # Stop the algorithm if cancel button has been clicked
-            if feedback.isCanceled():
-                break
+        # for current, feature in enumerate(features):
+            # # Stop the algorithm if cancel button has been clicked
+            # if feedback.isCanceled():
+                # break
 
-            # Add a feature in the sink
-            sink.addFeature(feature, QgsFeatureSink.FastInsert)
+            # # Add a feature in the sink
+            # sink.addFeature(feature, QgsFeatureSink.FastInsert)
 
-            # Update the progress bar
-            feedback.setProgress(int(current * total))
+            # # Update the progress bar
+            # feedback.setProgress(int(current * total))
 
-        # Return the results of the algorithm. In this case our only result is
-        # the feature sink which contains the processed features, but some
-        # algorithms may return multiple feature sinks, calculated numeric
-        # statistics, etc. These should all be included in the returned
-        # dictionary, with keys matching the feature corresponding parameter
-        # or output names.
-        return {self.OUTPUT: dest_id}
+        # # Return the results of the algorithm. In this case our only result is
+        # # the feature sink which contains the processed features, but some
+        # # algorithms may return multiple feature sinks, calculated numeric
+        # # statistics, etc. These should all be included in the returned
+        # # dictionary, with keys matching the feature corresponding parameter
+        # # or output names.
+        # return {self.OUTPUT: dest_id}
+        return {}
 
     def name(self):
         """
@@ -159,4 +151,4 @@ class DataStructureAlgorithm(QgsProcessingAlgorithm):
         return QCoreApplication.translate('Processing', string)
 
     def createInstance(self):
-        return MPConfigAlgorithm()
+        return DataStructureAlgorithm()
